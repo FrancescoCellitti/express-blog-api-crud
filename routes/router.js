c85040519d3e75372fc9dd58bb585e19e15297f7
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-01 13:32:36 +0200

    Refactor query handling and improve delete post logic in router

--- routes/router.js
+++ routes/router.js
@@ -9,7 +9,7 @@ const posts = require('../data/posts.js')
 
 router.get('/', (req, res) => {
     let filter = posts;
-    if(req.query.titolo){
+    if (req.query.titolo) {
         filter = posts.filter(
             post => post.titolo.toLowerCase().includes(req.query.titolo)
         )
@@ -18,7 +18,16 @@ router.get('/', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('cancellazione del post ' + req.params.id)
+    const id = parseInt(req.params.id)
+    const post = posts.find(post=> post.id === id)
+    console.log(posts)
+
+    if(!post){
+        res.status(204);
+        return res.json()
+    }
+
+    posts.splice(posts.indexOf(post), 1);
 })
 
 
