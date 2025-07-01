7bc200d3a06fc87870c7b208576967e5cb516376
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-01 14:16:50 +0200

    Refactor GET route to filter posts by tags and improve error handling

--- routes/router.js
+++ routes/router.js
@@ -9,18 +9,33 @@ const posts = require('../data/posts.js')
 
 router.get('/', (req, res) => {
     let filter = posts;
-    if (req.query.titolo) {
+    if (req.query.tags) {
         filter = posts.filter(
-            post => post.titolo.toLowerCase().includes(req.query.titolo)
+            post => post.tags.includes(req.query.tags)
         )
+    }
+     if (!posts.tags) {
+        res.status(404);
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Pizza non trovata"
+        })
     }
     res.json(filter)
 })
 
 router.delete('/', (req, res) => {
     const id = parseInt(req.params.id)
-    const post = posts.findIndex( post => post.titolo.toLowerCase().includes(req.query.titolo.toLowerCase()))
-  
+    const post = posts.findIndex(post => post.titolo.toLowerCase().includes(req.query.titolo.toLowerCase()))
+    if (post) {
+        res.status(404);
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Pizza non trovata"
+        })
+    }
 
     posts.splice(post, 1);
     console.log(posts)
