cadb63a406b7120bba2b07cad4933140f859d860
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-02 11:32:49 +0200

    Refactor router to use controller functions for GET and DELETE routes

--- routes/router.js
+++ routes/router.js
@@ -2,46 +2,15 @@ const express = require('express')
 const router = express.Router()
 const port = process.env.PORT
 const posts = require('../data/posts.js')
+const {filter, deleted}= require('../controllers/postsController.js')
 
 /* router.get('/', (req, res) => {
     res.json(posts)
 }) */
 
-router.get('/', (req, res) => {
-    let filter = posts;
-    if (req.query.tags) {
-        filter = posts.filter(
-            post => post.tags.includes(req.query.tags)
-        )
-    }
-     if (!posts.tags) {
-        res.status(404);
-        return res.json({
-            status: 404,
-            error: "Not Found",
-            message: "Pizza non trovata"
-        })
-    }
-    res.json(filter)
-})
+router.get('/', filter)
 
-router.delete('/', (req, res) => {
-    const id = parseInt(req.params.id)
-    const post = posts.findIndex(post => post.titolo.toLowerCase().includes(req.query.titolo.toLowerCase()))
-    if (post) {
-        res.status(404);
-        return res.json({
-            status: 404,
-            error: "Not Found",
-            message: "Pizza non trovata"
-        })
-    }
-
-    posts.splice(post, 1);
-    console.log(posts)
-    res.status(204)
-    res.end()
-})
+router.delete('/', deleted)
 
 
 module.exports = router
